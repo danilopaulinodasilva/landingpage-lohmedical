b05d51848bdd74312ca630f84e933ecc340309ee
Danilo Paulino da Silva <danilopaulinodasilva@MacBook-Air-de-Danilo.local>
2024-05-12 21:18:50 -0300

    fix foreach url

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -22,6 +22,7 @@ function addUrlToInput() {
   var urlInputs = document.querySelectorAll('input[name="url"]');
   if (urlInputs) {
     urlInputs.forEach(function (input) {
+      console.log("Adding ", window.location.href);
       input.value = window.location.href;
     });
   }
