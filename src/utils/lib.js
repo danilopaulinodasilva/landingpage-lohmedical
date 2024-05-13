95fe96a4821babeabc7157bdeccdbe51350edbab
Danilo Paulino da Silva <danilopaulinodasilva@MacBook-Air-de-Danilo.local>
2024-05-12 21:20:33 -0300

    fix?

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -1,23 +1,3 @@
-function addHiddenInput(value) {
-  const forms = document.querySelectorAll("form");
-  forms.forEach((form) => {
-    const input = document.createElement("input");
-    input.type = "hidden";
-    input.value = value;
-    form.appendChild(input);
-  });
-}
-
-function isSMTP() {
-  console.log("Using isSMTP()");
-  addHiddenInput("smtp");
-}
-
-function isMail() {
-  console.log("Using isMail()");
-  addHiddenInput("mail");
-}
-
 function addUrlToInput() {
   var urlInputs = document.querySelectorAll('input[name="url"]');
   if (urlInputs) {
